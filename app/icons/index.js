02ff9da097f9eef7676973a4f52ab285790194b9
Marko Stijak <mstijak@gmail.com>
2022-09-11 22:10:56 +0200

    Upgrade to Heroicons 2

--- app/icons/index.js
+++ app/icons/index.js
@@ -1,65 +1,63 @@
 import { Icon } from 'cx/widgets';
 import { VDOM } from 'cx/ui';
 import {
-   AdjustmentsIcon,
+   AdjustmentsHorizontalIcon,
    ArrowDownIcon,
    ArrowLeftIcon,
    ArrowUpIcon,
    CalendarIcon,
-   CashIcon,
+   BanknotesIcon,
    ChartBarIcon,
    ChevronDownIcon,
    CogIcon,
    CreditCardIcon,
    CurrencyDollarIcon,
    CurrencyEuroIcon,
-   DocumentReportIcon,
+   DocumentChartBarIcon,
    DocumentTextIcon,
-   ExclamationIcon,
+   ShieldExclamationIcon,
    InformationCircleIcon,
    PencilIcon,
    PlusIcon,
    PresentationChartBarIcon,
    PrinterIcon,
-   PuzzleIcon,
-   RefreshIcon,
-   SearchIcon,
-   TemplateIcon,
+   PuzzlePieceIcon,
+   ArrowPathIcon,
+   MagnifyingGlassIcon,
+   RectangleGroupIcon,
    UserGroupIcon,
    UserIcon,
    UsersIcon,
-   ViewListIcon,
-   XIcon,
-} from '@heroicons/react/outline/esm';
+   Bars4Icon,
+   XMarkIcon,
+} from '@heroicons/react/24/outline';
 
 //register all icons that are used within the application
 
 Icon.register('chart-bar', (props) => <ChartBarIcon {...props} />);
 
-Icon.register('adjustments', (props) => <AdjustmentsIcon {...props} />);
+Icon.register('adjustments', (props) => <AdjustmentsHorizontalIcon {...props} />);
 
-Icon.register('document-report', (props) => <DocumentReportIcon {...props} />);
+Icon.register('document-report', (props) => <DocumentChartBarIcon {...props} />);
 
-Icon.register('view-list', (props) => <ViewListIcon {...props} />);
+Icon.register('view-list', (props) => <Bars4Icon {...props} />);
 
 Icon.register('presentation-chart-bar', (props) => <PresentationChartBarIcon {...props} />);
 
-Icon.register('document-report', (props) => <DocumentReportIcon {...props} />);
-
-Icon.register('search', (props) => <SearchIcon {...props} />);
+Icon.register('search', (props) => <MagnifyingGlassIcon {...props} />);
 
 Icon.register('calendar', (props) => <CalendarIcon {...props} />);
 
-Icon.register('template', (props) => <TemplateIcon {...props} />);
+Icon.register('template', (props) => <RectangleGroupIcon {...props} />);
 
-Icon.register('puzzle', (props) => <PuzzleIcon {...props} />);
+Icon.register('puzzle', (props) => <PuzzlePieceIcon {...props} />);
 
-Icon.register('cash', (props) => <CashIcon {...props} />);
+Icon.register('cash', (props) => <BanknotesIcon {...props} />);
 
 Icon.register('arrow-up', (props) => <ArrowUpIcon {...props} />);
 Icon.register('arrow-down', (props) => <ArrowDownIcon {...props} />);
 
-Icon.register('exclamation', (props) => <ExclamationIcon {...props} />);
+Icon.register('exclamation', (props) => <ShieldExclamationIcon {...props} />);
 
 Icon.register('credit-card', (props) => <CreditCardIcon {...props} />);
 
@@ -84,10 +82,10 @@ Icon.register('drop-down', (props) => <ChevronDownIcon {...props} />);
 
 Icon.register('information-circle', (props) => <InformationCircleIcon {...props} />);
 
-Icon.register('refresh', (props) => <RefreshIcon {...props} />);
+Icon.register('refresh', (props) => <ArrowPathIcon {...props} />);
 
-Icon.register('x', (props) => <XIcon {...props} />);
-Icon.register('close', (props) => <XIcon {...props} />);
+Icon.register('x', (props) => <XMarkIcon {...props} />);
+Icon.register('close', (props) => <XMarkIcon {...props} />);
 
 Icon.register('plus', (props) => <PlusIcon {...props} />);
 
