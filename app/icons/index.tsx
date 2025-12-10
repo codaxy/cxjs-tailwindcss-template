0b209988a7c13c013a1bf215bf0d9ba97d05d801
Marko Stijak <marko.stijak@codaxy.com>
2025-12-10 09:35:26 +0100

    ...

--- app/icons/index.tsx
+++ app/icons/index.tsx
@@ -35,63 +35,61 @@ import {
 
 //register all icons that are used within the application
 
-Icon.register('chart-bar', (props) => <ChartBarIcon {...props} />);
+Icon.register('chart-bar', ({ key, ...rest }) => <ChartBarIcon key={key} {...rest} />);
 
-Icon.register('adjustments', (props) => <AdjustmentsHorizontalIcon {...props} />);
+Icon.register('adjustments', ({ key, ...rest }) => <AdjustmentsHorizontalIcon key={key} {...rest} />);
 
-Icon.register('document-report', (props) => <DocumentChartBarIcon {...props} />);
+Icon.register('document-report', ({ key, ...rest }) => <DocumentChartBarIcon key={key} {...rest} />);
 
-Icon.register('view-list', (props) => <Bars4Icon {...props} />);
+Icon.register('view-list', ({ key, ...rest }) => <Bars4Icon key={key} {...rest} />);
 
-Icon.register('presentation-chart-bar', (props) => <PresentationChartBarIcon {...props} />);
+Icon.register('presentation-chart-bar', ({ key, ...rest }) => <PresentationChartBarIcon key={key} {...rest} />);
 
-Icon.register('search', (props) => <MagnifyingGlassIcon {...props} />);
+Icon.register('search', ({ key, ...rest }) => <MagnifyingGlassIcon key={key} {...rest} />);
 
-Icon.register('calendar', (props) => <CalendarIcon {...props} />);
+Icon.register('calendar', ({ key, ...rest }) => <CalendarIcon key={key} {...rest} />);
 
-Icon.register('template', (props) => <RectangleGroupIcon {...props} />);
+Icon.register('template', ({ key, ...rest }) => <RectangleGroupIcon key={key} {...rest} />);
 
-Icon.register('puzzle', (props) => <PuzzlePieceIcon {...props} />);
+Icon.register('puzzle', ({ key, ...rest }) => <PuzzlePieceIcon key={key} {...rest} />);
 
-Icon.register('cash', (props) => <BanknotesIcon {...props} />);
+Icon.register('cash', ({ key, ...rest }) => <BanknotesIcon key={key} {...rest} />);
 
-Icon.register('arrow-up', (props) => <ArrowUpIcon {...props} />);
-Icon.register('arrow-down', (props) => <ArrowDownIcon {...props} />);
+Icon.register('arrow-up', ({ key, ...rest }) => <ArrowUpIcon key={key} {...rest} />);
+Icon.register('arrow-down', ({ key, ...rest }) => <ArrowDownIcon key={key} {...rest} />);
 
-Icon.register('exclamation', (props) => <ShieldExclamationIcon {...props} />);
+Icon.register('exclamation', ({ key, ...rest }) => <ShieldExclamationIcon key={key} {...rest} />);
 
-Icon.register('credit-card', (props) => <CreditCardIcon {...props} />);
+Icon.register('credit-card', ({ key, ...rest }) => <CreditCardIcon key={key} {...rest} />);
 
-Icon.register('document-text', (props) => <DocumentTextIcon {...props} />);
+Icon.register('document-text', ({ key, ...rest }) => <DocumentTextIcon key={key} {...rest} />);
 
-Icon.register('cog', (props) => <CogIcon {...props} />);
+Icon.register('cog', ({ key, ...rest }) => <CogIcon key={key} {...rest} />);
 
-Icon.register('adjustments', (props) => <AdjustmentsHorizontalIcon {...props} />);
+Icon.register('users', ({ key, ...rest }) => <UsersIcon key={key} {...rest} />);
 
-Icon.register('users', (props) => <UsersIcon {...props} />);
+Icon.register('user', ({ key, ...rest }) => <UserIcon key={key} {...rest} />);
 
-Icon.register('user', (props) => <UserIcon {...props} />);
+Icon.register('user-group', ({ key, ...rest }) => <UserGroupIcon key={key} {...rest} />);
 
-Icon.register('user-group', (props) => <UserGroupIcon {...props} />);
+Icon.register('currency-dollar', ({ key, ...rest }) => <CurrencyDollarIcon key={key} {...rest} />);
 
-Icon.register('currency-dollar', (props) => <CurrencyDollarIcon {...props} />);
+Icon.register('currency-euro', ({ key, ...rest }) => <CurrencyEuroIcon key={key} {...rest} />);
 
-Icon.register('currency-euro', (props) => <CurrencyEuroIcon {...props} />);
+Icon.register('chevron-down', ({ key, ...rest }) => <ChevronDownIcon key={key} {...rest} />);
+Icon.register('drop-down', ({ key, ...rest }) => <ChevronDownIcon key={key} {...rest} />);
 
-Icon.register('chevron-down', (props) => <ChevronDownIcon {...props} />);
-Icon.register('drop-down', (props) => <ChevronDownIcon {...props} />);
+Icon.register('information-circle', ({ key, ...rest }) => <InformationCircleIcon key={key} {...rest} />);
 
-Icon.register('information-circle', (props) => <InformationCircleIcon {...props} />);
+Icon.register('refresh', ({ key, ...rest }) => <ArrowPathIcon key={key} {...rest} />);
 
-Icon.register('refresh', (props) => <ArrowPathIcon {...props} />);
+Icon.register('x', ({ key, ...rest }) => <XMarkIcon key={key} {...rest} />);
+Icon.register('close', ({ key, ...rest }) => <XMarkIcon key={key} {...rest} />);
 
-Icon.register('x', (props) => <XMarkIcon {...props} />);
-Icon.register('close', (props) => <XMarkIcon {...props} />);
+Icon.register('plus', ({ key, ...rest }) => <PlusIcon key={key} {...rest} />);
 
-Icon.register('plus', (props) => <PlusIcon {...props} />);
+Icon.register('arrow-left', ({ key, ...rest }) => <ArrowLeftIcon key={key} {...rest} />);
 
-Icon.register('arrow-left', (props) => <ArrowLeftIcon {...props} />);
+Icon.register('printer', ({ key, ...rest }) => <PrinterIcon key={key} {...rest} />);
 
-Icon.register('printer', (props) => <PrinterIcon {...props} />);
-
-Icon.register('pencil', (props) => <PencilIcon {...props} />);
+Icon.register('pencil', ({ key, ...rest }) => <PencilIcon key={key} {...rest} />);
